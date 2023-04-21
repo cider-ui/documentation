5a9d663fd91cdf35aef851caa25847f0463912cc
Theo Marinho <thmarinho@outlook.fr>
2023-04-21 15:40:01 +0200

    feat: Init repo using tailwind template

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-  <header className='w-full h-14 flex justify-between px-4 py-2'>
-    <img src="logo192.png" alt="" className="h-full" />
-  </header>
-);
-
-export default Header;
